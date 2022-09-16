83ca108354ef59b8f7bf138f9f192308972076b4
Houser97 <arturo.riverar97@gmail.com>
2022-09-16 13:01:56 -0500

    Implement User modular route

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/', (req, res) => {
+    return res.send(Object.values(req.context.models.users));
+});
+
+router.get('/:userId', (req, res) => {
+    return res.send(req.context.models.users[req.params.userId]);
+});
+
+export default router;
